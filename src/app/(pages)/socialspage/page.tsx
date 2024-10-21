1a9e5640847b13b0b049645db032339ead6f6fb3
Odin-Johan <nidojohan@gmail.com>
2024-10-21 08:19:54 +0200

    top links work and redirect user to new page. Also added images of renders to the project

---
+++ src/app/(pages)/socialspage/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import React from "react";
+import Link from 'next/link';
+//import Image from "next/image";
+
+export default function Home() {
+const mainText = "Socials"
+    const textDesc = "Here are some of my socials"
+  return (
+    <main className="text-white">
+      <div className="flex gap-5 md:flex-row absolute top-5 right-5">
+      <Link href='page2' className="justify-center items-center text-[2vw] font-bold md:pr-10 md:pl-10 md:flex md:justify-center md:items-center md:text-[1vw] md:font-bold">Summary</Link>
+      <Link href='/' className="justify-center items-center text-[2vw] font-bold md:pr-10 md:pl-10 md:flex md:justify-center md:items-center md:text-[1vw] md:font-bold">Home</Link>
+      <Link href='futureprojects' className="justify-center items-center text-[2vw] font-bold md:pr-10 md:pl-10 md:flex md:justify-center md:items-center md:text-[1vw] md:font-bold">Future projects</Link>
+      </div>
+      <h1 className=" h-[45vh] flex justify-center items-center text-[15vw] font-bold md:h-[45vh] md:flex md:justify-center md:items-center md:text-[15vw] md:font-bold ">{mainText}</h1>
+      <p className="h-[10vh] flex justify-center pr-20 pl-20 items-center text-[5vw] font-bold md:h-[20vh] md:flex md:justify-center md:pr-80 md:pl-80 md:items-center md:text-[2vw] md:font-bold">{textDesc} </p>
+      <footer>
+        
+      </footer>
+      
+      
+      
+
+      
+      
+      
+    
+
+
+    </main>
+  );
+}
