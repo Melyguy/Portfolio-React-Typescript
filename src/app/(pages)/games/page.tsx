9967e6c14cc7743f6552ad71f57315145141406c
MellyDev <nidojohan@gmail.com>
2025-02-27 17:00:15 +0100

    unity almost working

--- src/app/(pages)/games/page.tsx
+++ src/app/(pages)/games/page.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 
 import React, {useEffect} from "react";
 import Lenis from "lenis";
+import UnityWebGL from "@/app/components/UnityWebgl";
 
 export default function Home() {
   useEffect( () => {
@@ -21,7 +22,7 @@ const mainText = "Games"
       <h1 className=" h-[20vh] flex justify-center items-center text-[15vw] font-bold md:h-[45vh] md:flex md:justify-center md:items-center md:text-[15vw] md:font-bold ">{mainText}</h1>
       <p className="h-[10vh] flex justify-center p-20items-center text-[5vw] font-bold md:flex md:justify-center md:pr-80 md:pl-80 md:items-center md:text-[2vw] md:font-bold">{textDesc} </p>
     
-      
+      <UnityWebGL />
 
     </main>
   );
