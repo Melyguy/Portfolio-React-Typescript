9eb24ab01d845209d27e5bd2e465e4e73bd64eeb
melyguy <nidojohan@gmail.com>
2025-08-26 11:07:10 +0200

    updatesss

--- src/app/components/NewNavbar.tsx
+++ src/app/components/NewNavbar.tsx
@@ -9,10 +9,21 @@ export default function NewNavbar(){
             <div className="w-full h-full flex items-center">
                 <Link href="/"  className="flex justify-center font-semibold text-md text-blue-950 hover:underline hover:underline-offset-8 hover:text-neutral-950 md:text-xl">3XM</Link>
             <div className="h-12 bg-neutral-300 w-2 ml-10 rounded-full md:flex hidden"></div>
-            <div className="h-full w-1/2 grid grid-cols-4 grid-rows-1 items-center p-10">
-                <Link href="/projects" className="col-span-1 row-span-1 text-lg font-extralight text-neutral-600 hover:underline">Projects</Link>
-                <Link href="/about" className="col-span-2 row-span-1 text-lg font-extralight text-neutral-600 hover:underline">Games and Mods</Link>
-
+            <div className="h-full w-1/2 grid grid-cols-4 grid-rows-1 items-center">
+                <div className="col-span-1 row-span-1 w-full ml-9">
+                    <Link href="/about" className=" text-sm font-extralight hidden md:flex text-neutral-600 hover:underline hover:underline-offset-8 md:text-lg">Projects</Link>
+                </div>
+                <div className="col-span-1 row-span-1 w-full hidden md:flex ml-9">
+                    <Link href="/projects" className=" text-sm font-extralight text-neutral-600 hover:underline hover:underline-offset-8 md:text-lg">Art</Link>
+                </div>
+                <div className="col-span-1 row-span-1 w-full hidden md:flex ml-9">
+                    <Link href="/projects" className=" text-sm font-extralight text-neutral-600 hover:underline hover:underline-offset-8 md:text-lg">Music
+                    
+                    </Link>
+                </div>
+                <div className="col-span-1 row-span-1 w-full hidden md:flex ml-9">
+                    <Link href="/projects" className=" text-sm font-extralight text-neutral-600 hover:underline hover:underline-offset-8 md:text-lg">Side projects</Link>
+                </div>
             </div>
             </div>
         </div>
